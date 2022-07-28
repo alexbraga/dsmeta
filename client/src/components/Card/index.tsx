e65853da4aa924c603f1fc2db852033b986f11af
Alex Braga <contato@alexbraga.com.br>
2022-07-28 05:39:20 -0300

    Implement useState hook on DatePicker

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,25 +1,32 @@
 import "./styles.css";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 import NotificationButton from "../NotificationButton";
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 
 function Card() {
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
+  const max = new Date();
+
+  const [minDate, setMinDate] = useState(min);
+  const [maxDate, setMaxDate] = useState(max);
+
   return (
     <div className="card">
       <h2>Sales</h2>
       <div>
         <div className="form-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => {}}
+            selected={minDate}
+            onChange={(date: Date) => setMinDate(date)}
             className="form-control"
             dateFormat="dd/MM/yyyy"
           />
         </div>
         <div className="form-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => {}}
+            selected={maxDate}
+            onChange={(date: Date) => setMaxDate(date)}
             className="form-control"
             dateFormat="dd/MM/yyyy"
           />
