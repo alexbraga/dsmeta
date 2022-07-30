fa4bb6ef5c21ab923e153dcad4767bb132a2eee6
Alex Braga <contato@alexbraga.com.br>
2022-07-30 06:38:05 -0300

    Add successful SMS sending feedback on screen via react-toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Card from "./components/Card";
 import Header from "./components/Header/index";
 
 function App() {
   return (
     <>
+      <ToastContainer autoClose={5000} />
       <Header />
       <main>
         <section id="sales">
