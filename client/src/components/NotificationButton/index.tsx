85ac8e6f1849db29045b148bf90b2bd5ee494763
Alex Braga <contato@alexbraga.com.br>
2024-02-27 05:30:03 -0300

    Improve toast notification handling error

--- client/src/components/NotificationButton/index.tsx
+++ client/src/components/NotificationButton/index.tsx
@@ -9,9 +9,14 @@ type Props = {
 };
 
 function handleClick(id: number) {
-  axios
-    .get(`${BASE_URL}/sales/${id}/notification`)
-    .then((response) => toast.info("SMS sent successfully!"));
+  axios.get(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    if (response.status === 200) {
+      toast.success("SMS sent successfully!");
+    } else if (response.status === 500) {
+      toast.error("Something went wrong. Please try again.");
+    }
+    // Add more conditions if needed for other status codes
+  });
 }
 
 function NotificationButton({ saleId }: Props) {
